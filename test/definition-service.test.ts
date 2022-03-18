89b0c8c585bdaccb7016b2cff344b12e21d72633
josnih21 <josnih@gmail.com>
2022-03-18 13:19:37 +0000

    :white_check_mark: Green test use cases for definition-service

---
+++ test/definition-service.test.ts
@@ -0,0 +1,41 @@
+import {
+	DefinitionService,
+	RaeApiDefinitionService,
+} from "../src/definition-service";
+import {RAE} from "rae-api";
+import {NotDefinitionFoundError} from "../src/errors";
+import {DefinitionEntity} from "../src/definition-entity";
+
+const raeApi = new RAE();
+const definitionService: DefinitionService = new RaeApiDefinitionService(
+	raeApi
+);
+
+test("should throw an error when no definition found", () => {
+	const anUnreachableWord: string = "sdgdsgdsgdsgsdg";
+
+	const expectedResult = () =>
+		definitionService.findDefinitionsFor(anUnreachableWord);
+
+	expect(expectedResult).rejects.toThrow(NotDefinitionFoundError);
+});
+
+test("should return a definition given a valid word", () => {
+	const aWord: string = "hola";
+
+	const definitions = definitionService.findDefinitionsFor(aWord);
+
+	const expectedResult = [
+		new DefinitionEntity("interjección", "U. como salutación familiar."),
+		new DefinitionEntity(
+			"interjección",
+			"poco us. U. para denotar extrañeza, placentera o desagradable. U. también repetida."
+		),
+		new DefinitionEntity(
+			"interjección",
+			"desus. Era u. para llamar a los inferiores."
+		),
+	];
+
+	expect(definitions).resolves.toStrictEqual(expectedResult)
+});
